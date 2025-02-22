33403f9e7618b748ac9362523bcfa97e9ce6d115
mojahige <13248811+mojahige@users.noreply.github.com>
2025-02-23 02:04:07 +0900

    Enable static middleware

--- routes/routes.go
+++ routes/routes.go
@@ -7,11 +7,16 @@ import (
 
 func SetupRoutes(e *echo.Echo) {
 	setupWebRoutes(e)
+	setupStaticRoutes(e)
 	setupErrorRoutes(e)
 }
 
 func setupWebRoutes(e *echo.Echo) {
 	e.GET("/", handlers.HomeHandler)
+}
+
+func setupStaticRoutes(e *echo.Echo) {
+	e.Static("/assets", "assets")
 }
 
 func setupErrorRoutes(e *echo.Echo) {
